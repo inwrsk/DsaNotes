bec61c5210e20acd66e06d200fd90a54f2db59db
inwrsk <inwrsk@gmail.com>
2025-12-16 20:49:08 +0530

    Revised upto Oops

--- C_Strings/A_StringBuilder.java
+++ C_Strings/A_StringBuilder.java
@@ -11,9 +11,11 @@ public class A_StringBuilder {
         sb.deleteCharAt(sb.length() - 1);// deleting the last character
         sb.insert(sb.length(), "d");// inserting a char at the last position
         System.out.println(sb + " " + sb.getClass().getName());// abd java.lang.StringBuilder
+
+	sb.insert(sb.length()-1, "c"); // inserts the string we give from that position and moves the other to the right side!
         String str = sb.toString();
         // toString is applied on objects
-        System.out.println(str + " " + str.getClass().getName());// abd java.lang.String
+        System.out.println(str + " " + str.getClass().getName());// abcd java.lang.String
         System.out.println(s2==sb);// true even after modifying sb, cz they are pointing to same object
     }
 }
